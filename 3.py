d6fc3be6ba029ebc52548643834cca2736923b4c
vasiliypismenn <115738755+vasiliypismenn@users.noreply.github.com>
2023-02-02 20:24:11 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,7 @@
+#46. Найти произведение пар чисел списка
+#(парой считаем первый и последний, второй предпоследний и тд)
+
+import math
+list_a = list(map(int, input('Введите числа, через пробел: ').split()))
+print('Исходный список: ',list_a)
+print('Результат: ',list([a*b for a,b in zip(list_a[0:math.ceil(len(list_a)/2)],list_a[::-1])]))
